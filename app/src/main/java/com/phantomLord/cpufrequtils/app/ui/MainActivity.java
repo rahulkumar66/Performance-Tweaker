708d87de122d66b06441cdd1aaf012e4fa117b63
rahul kumar <rahulkumar2048@gmail.com>
2015-08-10 22:07:10 +0530

    fix #12:check for busybox support on app startup

--- app/src/main/java/com/phantomLord/cpufrequtils/app/ui/MainActivity.java
+++ app/src/main/java/com/phantomLord/cpufrequtils/app/ui/MainActivity.java
@@ -13,13 +13,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.phantomLord.cpufrequtils.app.R;
-import com.phantomLord.cpufrequtils.app.dialogs.RootNotFoundAlertDialog;
 import com.phantomLord.cpufrequtils.app.utils.GpuUtils;
 import com.phantomLord.cpufrequtils.app.utils.SysUtils;
+import com.stericson.RootTools.RootTools;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     private Context context;
 
@@ -29,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
     private ActionBarDrawerToggle mDrawerToggle;
     private ActionBar actionBar;
     private NavigationView navigationView;
+    private TextView appCompatibilityMessage;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,81 +43,100 @@ public class MainActivity extends AppCompatActivity {
         navigationView = (NavigationView) findViewById(R.id.navigation);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        appCompatibilityMessage = (TextView) findViewById(R.id.app_compatibility_status);
+        progressBar = (ProgressBar) findViewById(R.id.loading_main);
+
         setSupportActionBar(toolbar);
 
         // BugSenseHandler.initAndStartSession(MainActivity.this, "4cdc31a1");
         context = getBaseContext();
         actionBar = getSupportActionBar();
 
-        if (!(SysUtils.isRooted()))
-            new RootNotFoundAlertDialog().show(getSupportFragmentManager(),
-                    getString(R.string.app_name));
+        progressBar.setVisibility(View.VISIBLE);
 
+        if (!(SysUtils.isRooted())) {
+            progressBar.setVisibility(View.GONE);
 
-        if (GpuUtils.isGpuFrequencyScalingSupported()) {
-            navigationView.getMenu().getItem(1).setVisible(true);
+            appCompatibilityMessage.setVisibility(View.VISIBLE);
+            appCompatibilityMessage.setText("No Root Found");
         }
 
-        this.getFragmentManager().beginTransaction()
-                .replace(R.id.main_content, new CpuFrequencyFragment())
-                .commit();
+        if (!(RootTools.isBusyboxAvailable())) {
+            progressBar.setVisibility(View.GONE);
 
+            appCompatibilityMessage.setVisibility(View.VISIBLE);
+            appCompatibilityMessage.setText("No BusyBox Found");
+        } else {
 
-        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.action_settings,
-                R.string.action_settings) {
-            @Override
-            public void onDrawerOpened(View drawerView) {
-                super.onDrawerOpened(drawerView);
+            progressBar.setVisibility(View.GONE);
+
+            if (GpuUtils.isGpuFrequencyScalingSupported()) {
+                navigationView.getMenu().getItem(1).setVisible(true);
             }
 
-            @Override
-            public void onDrawerClosed(View drawerView) {
-                super.onDrawerClosed(drawerView);
-            }
-        };
-        mDrawerToggle.syncState();
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(MenuItem menuItem) {
-                Fragment mfragment = null;
-                switch (menuItem.getItemId()) {
-                    case R.id.nav_cpu:
-                        mfragment = new CpuFrequencyFragment();
-                        actionBar.setTitle("Cpu Frequency");
-                        break;
-                    case R.id.nav_tis:
-                        mfragment = new TimeInStatesFragment();
-                        actionBar.setTitle("Time In State");
-                        break;
-                    case R.id.nav_iocontrol:
-                        mfragment = new IOControlFragment();
-                        actionBar.setTitle("I/O Control");
-                        break;
-                    case R.id.nav_wakelocks:
-                        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-                        mfragment = new WakeLocksFragment();
-                        actionBar.setTitle("Wakelocks");
-                        break;
-                    case R.id.nav_settings:
-                        mfragment = new SettingsFragment();
-                        actionBar.setTitle(getString(R.string.action_settings));
-                        break;
-                    case R.id.nav_gpu:
-                        mfragment = new GpuControlFragment();
-                        actionBar.setTitle("GPU Settings");
-                        break;
+            mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.action_settings,
+                    R.string.action_settings) {
+                @Override
+                public void onDrawerOpened(View drawerView) {
+                    super.onDrawerOpened(drawerView);
                 }
-                if (mfragment != null) {
-                    getFragmentManager().beginTransaction()
-                            .replace(R.id.main_content, mfragment)
-                            .commit();
 
+                @Override
+                public void onDrawerClosed(View drawerView) {
+                    super.onDrawerClosed(drawerView);
                 }
-                mDrawerLayout.closeDrawer(GravityCompat.START);
-                return true;
-            }
+            };
+            mDrawerToggle.syncState();
+            navigationView.setNavigationItemSelectedListener(this);
 
-        });
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.main_content, new CpuFrequencyFragment())
+                    .commit();
+        }
+
     }
 
+    @Override
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
+        Fragment mfragment = null;
+
+        switch (menuItem.getItemId()) {
+            case R.id.nav_cpu:
+                mfragment = new CpuFrequencyFragment();
+                actionBar.setTitle("Cpu Frequency");
+                break;
+            case R.id.nav_tis:
+                mfragment = new TimeInStatesFragment();
+                actionBar.setTitle("Time In State");
+                break;
+            case R.id.nav_iocontrol:
+                mfragment = new IOControlFragment();
+                actionBar.setTitle("I/O Control");
+                break;
+            case R.id.nav_wakelocks:
+                actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+                mfragment = new WakeLocksFragment();
+                actionBar.setTitle("Wakelocks");
+                break;
+            case R.id.nav_settings:
+                mfragment = new SettingsFragment();
+                actionBar.setTitle(getString(R.string.action_settings));
+                break;
+            case R.id.nav_gpu:
+                mfragment = new GpuControlFragment();
+                actionBar.setTitle("GPU Settings");
+                break;
+        }
+
+        if (mfragment != null) {
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.main_content, mfragment)
+                    .commit();
+
+        }
+
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
 }
