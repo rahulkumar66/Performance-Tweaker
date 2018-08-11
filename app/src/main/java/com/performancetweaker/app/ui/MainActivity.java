f8dfb6a5c5ed1510b84911dc7802152bb8f9a1ac
Rahul kumar <rahul.kumar@venturepact.com>
2018-08-11 11:47:16 +0530

    Fix gradle issues

--- app/src/main/java/com/performancetweaker/app/ui/MainActivity.java
+++ app/src/main/java/com/performancetweaker/app/ui/MainActivity.java
@@ -13,12 +13,12 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
 
 import com.performancetweaker.app.BuildConfig;
 import com.performancetweaker.app.R;
