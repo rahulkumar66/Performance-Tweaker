cf08a75d46fd9b403ee6e27443d1653881a775b7
Rahul kumar <rahulkumar2048@gmail.com>
2018-10-01 16:57:58 +0530

    Add Crashlytics and update to android Pie

--- app/src/main/java/com/performancetweaker/app/PerfTweakerApplication.java
+++ app/src/main/java/com/performancetweaker/app/PerfTweakerApplication.java
@@ -2,6 +2,8 @@ package com.performancetweaker.app;
 
 import android.app.Application;
 import android.content.Context;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class PerfTweakerApplication extends Application {
     private static Context context;
@@ -9,6 +11,7 @@ public class PerfTweakerApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         PerfTweakerApplication.context = getApplicationContext();
     }
 
