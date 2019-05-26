2e4db875284432415e3f397f1fa77b24e6627164
Rahul Kumar <rahulkumar2048@gmail.com>
2019-05-26 21:05:11 +0530

    Fix apply on boot on post oreo devices

--- app/src/main/java/com/performancetweaker/app/services/BootService.java
+++ app/src/main/java/com/performancetweaker/app/services/BootService.java
@@ -1,9 +1,14 @@
 package com.performancetweaker.app.services;
 
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.IBinder;
 import android.preference.PreferenceManager;
 import android.util.Log;
 
@@ -20,13 +25,45 @@ public class BootService extends IntentService {
 
     SharedPreferences prefs;
     Context context;
+    static final String CHANNEL_ID = "onbootNotificationChannel";
+    static final int ON_BOOT_NOTIFICATION_ID = 1;
 
     public BootService() {
         super("Boot Service");
     }
 
     @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
     protected void onHandleIntent(Intent intent) {
+
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager notificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID,
+                    getString(R.string.apply_on_boot), NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.setSound(null, null);
+            notificationManager.createNotificationChannel(notificationChannel);
+
+            Notification.Builder builder = new Notification.Builder(this, CHANNEL_ID);
+            builder.setContentTitle(getString(R.string.apply_on_boot))
+                    .setSmallIcon(R.mipmap.ic_launcher);
+            startForeground(ON_BOOT_NOTIFICATION_ID, builder.build());
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
         context = getApplicationContext();
         prefs = PreferenceManager.getDefaultSharedPreferences(context);
         try {
@@ -59,7 +96,7 @@ public class BootService extends IntentService {
                     min = prefs.getString(Constants.PREF_GPU_MIN, null);
                     gov = prefs.getString(Constants.PREF_GPU_GOV, null);
                     Log.d("gpu", max + " " + min + " " + gov);
-                    GpuUtils gpuUtils= GpuUtils.getInstance();
+                    GpuUtils gpuUtils = GpuUtils.getInstance();
                     if (max != null)
                         gpuUtils.setMaxGpuFrequency(max, context);
                     if (min != null)
@@ -84,31 +121,9 @@ public class BootService extends IntentService {
                 if (applyOnBootSet.contains(getString(R.string.build_prop))) {
 
                 }
-
-                //Log.d("he");
-         /*   if (prefs.getBoolean(Constants.PREF_CPU_APPLY_ON_BOOT, false)) {
-                String max, min, gov;
-                max = prefs.getString(Constants.PREF_MAX_FREQ, null);
-                min = prefs.getString(Constants.PREF_MIN_FREQ, null);
-                gov = prefs.getString(Constants.PREF_GOV, null);
-
-                if (max != null || min != null || gov != null) {
-
-                }
-            }
-            if (prefs.getBoolean(Constants.PREF_IO_APPLY_ON_BOOT, false)) {
-                String ioscheduler = prefs.getString(Constants.PREF_IO_SCHEDULER, null);
-                String readAhead = prefs.getString(Constants.PREF_READ_AHEAD, null);
-
-                if (ioscheduler != null || readAhead != null) {
-                    //   IOUtils.setDiskSchedulerandReadAhead(ioscheduler,
-                    //         readAhead, context);
-                }
-            }
-            */
+                stopSelf();
             }
         }
-
-        stopSelf();
+        return START_NOT_STICKY;
     }
 }
